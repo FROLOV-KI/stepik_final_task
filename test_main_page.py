4533949749f1c437a3b74b37e5ef8faecb2a4e66
Frolov Kirill <Frolov.Kirill.17@yandex.ru>
2021-03-21 21:12:20 +0300

    В рамках одного теста проверяется основная страница и логин-страница

--- test_main_page.py
+++ test_main_page.py
@@ -1,9 +1,14 @@
 from .pages.main_page import MainPage
+from .pages.login_page import LoginPage
 
 
 def test_guest_can_go_to_login_page(browser):
     link = "http://selenium1py.pythonanywhere.com/"
     page = MainPage(browser, link) # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
     page.open()                    # открываем страницу
-    page.should_be_login_link()
-    page.go_to_login_page()
+    page.should_be_login_link()    # проверяем наличие ссылки на логин-страницу
+    page.go_to_login_page()        # переходим на логин-страницу
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_url()      # проверяем, что перешли на нужную страницу
+    login_page.should_be_login_form()     # проверяем наличие формы логина
+    login_page.should_be_register_form()  # проверяем наличие формы регистрации
